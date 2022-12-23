d85882ffc55c2efeaa8c2261bee834a757361de2
juan-20 <juanalvesandrade@gmail.com>
2022-12-23 10:29:54 -0300

    removing gif

--- src/components/HeroImage/index.tsx
+++ src/components/HeroImage/index.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
-import me from '../../../public/Iconeanimado-Juan.gif'
+import me from '../../../public/eu.png'
 import { Container } from './styles'
 
 export default function HeroImage() {

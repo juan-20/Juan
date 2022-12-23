1f8de859a081ee72f8f6230c4ec30b4a55fc1a42
juan-20 <juanalvesandrade@gmail.com>
2022-12-23 09:42:58 -0300

    next config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['github.com', 'emojipedia-us.s3.dualstack.us-west-1.amazonaws.com']
-  }
+  },
+  "presets": ["next/babel"]
 }
 
 module.exports = nextConfig

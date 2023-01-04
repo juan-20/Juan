17e24c8461d1185f04c7c2437e46f88fdb9c78e5
juan-20 <juanalvesandrade@gmail.com>
2023-01-04 09:26:48 -0300

    adding &apos;

--- src/components/HeroText/index.tsx
+++ src/components/HeroText/index.tsx
@@ -8,7 +8,7 @@ export default function HeroText() {
       <h1>Hi, i am Juan</h1>
       <div className="social-text">
         <p>
-          I live in Brazil, I&apos;m 20 years old and I've been working as web developer since 2020 <br/>
+          I live in Brazil, I&apos;m 20 years old and I&apos;ve been working as web developer since 2020 <br/>
           I took a technical course at <span>Cotemig in web/mobile development</span> 
           . I&apos;ve been working at <span>Kukac</span> as a Front-End Development and Tester.
           ✏️

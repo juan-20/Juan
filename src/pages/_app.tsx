1128e571e58f480c902e7165586c085c14dadefe
juan-20 <juanalvesandrade@gmail.com>
2023-01-04 20:57:20 -0300

    system theme as default

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,20 +10,23 @@ import { Analytics } from '@vercel/analytics/react';
 
 
 function MyApp ({ Component, pageProps }: AppProps) {
-  const [test, settest] = useState<any>('');
+  const [system, setSystem] = useState<any>('');
   let def 
   useEffect(() => {
     const darkThemeMq: any = window.matchMedia("(prefers-color-scheme: dark)");
     def = darkThemeMq
-    settest(darkThemeMq.matches)
+    setSystem(darkThemeMq.matches)
   }),[]
   let SystemTheme
-  if(test === true){
-    SystemTheme = light
-  }else{
+  
+  if(system === true){
     SystemTheme = dark
+  }else{
+    SystemTheme = light
   }
-  const [theme, setTheme] = useState(SystemTheme);
+  console.log(system)
+  console.log(SystemTheme)
+  const [theme, setTheme] = useState(SystemTheme.title === 'dark' ? light : dark);
   const toggleTheme = () => {
     setTheme(theme.title === 'dark' ? light : dark)
   }
